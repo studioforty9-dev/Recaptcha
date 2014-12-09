5907c35c1aa7be6255b8f1ad0303fcf20c2908fd
Eoghan O'Brien <eoghan@eoghanobrien.com>
2014-12-09 10:45:34 +0000

    Fix translation method call

--- app/code/community/Studioforty9/Recaptcha/Model/Source/Theme.php
+++ app/code/community/Studioforty9/Recaptcha/Model/Source/Theme.php
@@ -10,8 +10,19 @@ class Studioforty9_Recaptcha_Model_Source_Theme
     public function toOptionArray()
     {
         return array(
-            'light' => $this->__('Light'),
-            'dark'  => $this->__('Dark')
+            'light' => $this->_getDataHelper()->__('Light'),
+            'dark'  => $this->_getDataHelper()->__('Dark')
         );
+    }
+
+    /**
+     * Fetch the data helper for the module.
+     *
+     * @codeCoverageIgnore
+     * @return Studioforty9_Recaptcha_Helper_Data
+     */
+    protected function _getDataHelper()
+    {
+        return Mage::helper('studioforty9_recaptcha');
     }
 }

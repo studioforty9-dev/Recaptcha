6c38fdd13d6e4945447e666f976670f23583c3dc
Raphael Petrini <raphael@factoryx.com.au>
2017-11-09 10:52:35 +0100

    missing parenthesis

--- app/code/community/Studioforty9/Recaptcha/Model/Backend/Routes.php
+++ app/code/community/Studioforty9/Recaptcha/Model/Backend/Routes.php
@@ -14,7 +14,7 @@ class Studioforty9_Recaptcha_Model_Backend_Routes extends Mage_Core_Model_Config
         if (in_array('checkout_onepage_index', $value)) {
             $value[] = 'checkout_onepage_saveBilling';
             $this->setValue($value);
-        } elseif ($key = array_search('checkout_onepage_saveBilling', $value)) !== false) {
+        } elseif (($key = array_search('checkout_onepage_saveBilling', $value)) !== false) {
             unset($value[$key]);
             $this->setValue($value);
         }
